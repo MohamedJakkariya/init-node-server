47e13b360bca5ac0d1d69f756e4aef0b911dc172
MohamedJakkariya <mohamed.jakkariya@fleksa.com>
2020-10-30 15:42:08 +0800

    Addec couple more dependencies and cmd --help logs

--- setup/createNodeSqlServer.js
+++ setup/createNodeSqlServer.js
@@ -1,23 +1,40 @@
 /** @format */
 
 const commander = require('commander');
+const chalk = require('chalk');
 
-const program = new commander.Command();
+const packageJson = require('./package.json');
+const program = new commander.Command(packageJson.name);
+
+const log = console.log;
 
 function init() {
-	program
-		.option('-d, --debug', 'output extra debugging')
-		.option('-s, --small', 'small pizza size')
-		.option('-p, --pizza-type <type>', 'flavour of pizza', 'normal');
+  program
+    .version(packageJson.version)
+    .arguments('<project-directory>')
+    .usage(`${chalk.green('<project-directory>')} [options]`)
+    .action(name => {
+      projectName = name;
+    })
+    .option('--info', 'print environment debug info')
+    .option('--template <path-to-template>', 'specify a template for the created project')
+    .allowUnknownOption()
+    .on('--help', () => {
+      log(`    Only ${chalk.green('<project-directory>')} is required.`);
+      log();
 
-	program.parse(process.argv);
+      log(
+        `      - a local path relative to the current working directory: ${chalk.green('file:../my-node-sql-template')}`
+      );
 
-	if (program.debug) console.log(program.opts());
-	console.log('pizza details:');
-	if (program.small) console.log('- small pizza size');
-	if (program.pizzaType) console.log(`- ${program.pizzaType}`);
+      log();
+      log(`    If you have any problems, do not hesitate to file an issue:`);
+      log(`      ${chalk.cyan('https://github.com/MohamedJakkariya/init-node-server/issues')}`);
+      log();
+    })
+    .parse(process.argv);
 }
 
 module.exports = {
-    init
-}
+  init
+};
