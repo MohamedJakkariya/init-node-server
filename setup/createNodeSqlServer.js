b5cb2ce6f7b01c846e65ee1eac454d52c08676a5
MohamedJakkariya <mohamed.jakkariya@fleksa.com>
2020-10-30 20:34:47 +0800

    added few more packages

--- setup/createNodeSqlServer.js
+++ setup/createNodeSqlServer.js
@@ -9,6 +9,8 @@ const https = require('https');
 const path = require('path');
 const validateProjectName = require('validate-npm-package-name');
 const fs = require('fs-extra');
+const os = require('os');
+const spawn = require('cross-spawn');
 
 const packageJson = require('./package.json');
 
@@ -120,7 +122,7 @@ function init() {
     });
 }
 
-function createNodeServer(name, verbose, npm) {
+function createNodeServer(name, verbose, useNpm) {
   const nodeVersion = 10;
   const unsupportedNodeVersion = !semver.satisfies(process.version, `>=${nodeVersion}`);
 
@@ -150,6 +152,38 @@ function createNodeServer(name, verbose, npm) {
 
   console.log(`Creating a new node-sql server in ${chalk.green(root)}.`);
   console.log();
+
+  const packageJson = {
+    name: serverName,
+    version: '0.1.0',
+    private: true
+  };
+
+  fs.writeFileSync(path.join(root, 'package.json'), JSON.stringify(packageJson, null, 2) + os.EOL);
+
+  const originalDirectory = process.cwd();
+
+  process.chdir(root);
+
+  //   Checking Npm support
+  if (!checkThatNpmCanReadCwd) process.exit(1);
+
+  //   Surely they're used npm check
+  if (useNpm) {
+    const npmInfo = checkNpmVersion();
+    if (!npmInfo.hasMinNpm) {
+      if (npmInfo.npmVersion) {
+        console.log(
+          chalk.yellow(
+            `You are using npm ${npmInfo.npmVersion} so the project will be bootstrapped with an old unsupported version of tools.\n\n` +
+              `Please update to npm 6 or higher for a better, fully supported experience.\n`
+          )
+        );
+      }
+      // Fall back to latest supported react-scripts for npm 3
+      version = 'react-scripts@0.9.x';
+    }
+  }
 }
 
 // Fetch latest version from npm dist-tag and validate with current version
@@ -266,6 +300,62 @@ function isSafeToCreateProjectIn(root, name) {
     }
   });
   return true;
+}
+
+// Check npm can read the cwd
+function checkThatNpmCanReadCwd() {
+  const cwd = process.cwd();
+  let childOutput = null;
+  try {
+    // Note: intentionally using spawn over exec since
+    // the problem doesn't reproduce otherwise.
+    // `npm config list` is the only reliable way I could find
+    // to reproduce the wrong path. Just printing process.cwd()
+    // in a Node process was not enough.
+    childOutput = spawn.sync('npm', ['config', 'list']).output.join('');
+  } catch (err) {
+    // Something went wrong spawning node.
+    // Not great, but it means we can't do this check.
+    // We might fail later on, but let's continue.
+    return true;
+  }
+  if (typeof childOutput !== 'string') {
+    return true;
+  }
+  const lines = childOutput.split('\n');
+  // `npm config list` output includes the following line:
+  // "; cwd = C:\path\to\current\dir" (unquoted)
+  // I couldn't find an easier way to get it.
+  const prefix = '; cwd = ';
+  const line = lines.find(line => line.startsWith(prefix));
+  if (typeof line !== 'string') {
+    // Fail gracefully. They could remove it.
+    return true;
+  }
+  const npmCWD = line.substring(prefix.length);
+  if (npmCWD === cwd) {
+    return true;
+  }
+  console.error(
+    chalk.red(
+      `Could not start an npm process in the right directory.\n\n` +
+        `The current directory is: ${chalk.bold(cwd)}\n` +
+        `However, a newly started npm process runs in: ${chalk.bold(npmCWD)}\n\n` +
+        `This is probably caused by a misconfigured system terminal shell.`
+    )
+  );
+  if (process.platform === 'win32') {
+    console.error(
+      chalk.red(`On Windows, this can usually be fixed by running:\n\n`) +
+        `  ${chalk.cyan('reg')} delete "HKCU\\Software\\Microsoft\\Command Processor" /v AutoRun /f\n` +
+        `  ${chalk.cyan('reg')} delete "HKLM\\Software\\Microsoft\\Command Processor" /v AutoRun /f\n\n` +
+        chalk.red(`Try to run the above two lines in the terminal.\n`) +
+        chalk.red(
+          `To learn more about this problem, read: https://blogs.msdn.microsoft.com/oldnewthing/20071121-00/?p=24433/`
+        )
+    );
+  }
+  return false;
 }
 
 module.exports = {
